6353563cc3925c8bc7027913a4ac8476744d95fd
Hosick <33890461+Hosick@users.noreply.github.com>
2020-01-26 17:09:15 +0900

    simple algorithm

---
+++ src/baekjoon_step/step10_Recursion/Code_10870.java
@@ -0,0 +1,20 @@
+package baekjoon_step.step10_Recursion;
+
+import java.util.Scanner;
+
+public class Code_10870 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        System.out.println(fibonacci(num));
+    }
+
+    public static int fibonacci(int num) {
+        if (num == 0)
+            return 0;
+        if (num == 1)
+            return 1;
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
