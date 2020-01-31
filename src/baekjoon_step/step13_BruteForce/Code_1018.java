4fbc2925be105456eac38ec2c009c9df27a87e2c
Hosick <ghtjr5000@naver.com>
2020-01-31 14:06:37 +0900

    brute force algorithm

--- src/baekjoon_step/step13_BruteForce/Code_1018.java
+++ src/baekjoon_step/step13_BruteForce/Code_1018.java
@@ -18,6 +18,7 @@ public class Code_1018 {
         int n = input.nextInt();
         int m = input.nextInt();
         char[][] board = new char[n][m];
+        int minCount = 64;
 
         /* n*m 크기의 보드 상태 입력*/
         for (int i = 0; i < n; ++i) {
@@ -26,9 +27,23 @@ public class Code_1018 {
                 board[i][j] = line.charAt(j);
             }
         }
-        
-
-
-
+        /*다시 칠해야 하는 정사각형 개수의 최솟값 계산하기*/
+        for (int i = 0; i < n - 7; ++i) {   //  보드안에서 8x8 시작위치 (i,j)
+            for (int j = 0; j < m - 7; ++j) {
+                int wCount = 0;
+                int bCount = 0;
+                for (int k = 0; k < 8; ++k) {   //  (i,j)부터 8x8 보드와 체스판과 비교하기
+                    for (int l = 0; l < 8; ++l) {
+                        if (board[i + k][j + l] == white[k][l])
+                            ++wCount;
+                        if (board[i + k][j + l] == black[k][l])
+                            ++bCount;
+                    }
+                }
+                if (wCount < minCount) minCount = wCount;
+                if (bCount < minCount) minCount = bCount;
+            }
+        }
+        System.out.print(minCount);
     }
 }
