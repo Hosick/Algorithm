8f1c849f7bf06f6635822c609c06bb938788e5ca
Hosick <ghtjr5000@naver.com>
2020-02-04 21:04:32 +0900

    backTracking algorithm

---
+++ src/baekjoon_step/step15_Backtracking/Code_14888.java
@@ -0,0 +1,66 @@
+package baekjoon_step.step15_Backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Code_14888 {
+    public static int max = -1000000001;
+    public static int min = 1000000001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());   //  입력할 숫자의 갯수
+
+        /*수열*/
+        st = new StringTokenizer(br.readLine());
+        int num[] = new int[n];
+        for (int i = 0; i < n; ++i)
+            num[i] = Integer.parseInt(st.nextToken());
+
+        /*연산자의 갯수 배열*/
+        st = new StringTokenizer(br.readLine());
+        int[] operator = new int[4];
+        for (int i = 0; i < 4; ++i)
+            operator[i] = Integer.parseInt(st.nextToken());
+
+        calc(num[0], 1, n, num, operator); //  정답 / 재귀 깊이 /  수열 내 수의 갯수 / 수열 배열 / 연산자 갯수 배열
+        System.out.print(max + "\n" + min);
+    }
+
+    private static void calc(int answer, int deep, int n, int[] num, int[] operator) {
+        /*남아 있는 연산자가 없다면 최소,최댓값과 비교후 return*/
+        if (operator[0] + operator[1] + operator[2] + operator[3] == 0) {
+            if (answer > max) max = answer;
+            if (answer < min) min = answer;
+            return;
+        }
+
+        if (operator[0] != 0) { //  더하기 연산
+            --operator[0];
+            calc(answer + num[deep], ++deep, n, num, operator);
+            ++operator[0];
+            --deep;
+        }
+        if (operator[1] != 0) { //  빼기 연산
+            --operator[1];
+            calc(answer - num[deep], ++deep, n, num, operator);
+            ++operator[1];
+            --deep;
+        }
+        if (operator[2] != 0) { //  곱하기 연산
+            --operator[2];
+            calc(answer * num[deep], ++deep, n, num, operator);
+            ++operator[2];
+            --deep;
+        }
+        if (operator[3] != 0) { //  나누기 연산
+            --operator[3];
+            calc(answer / num[deep], ++deep, n, num, operator);
+            ++operator[3];
+            --deep;
+        }
+    }
+}
---
+++ src/baekjoon_step/step15_Backtracking/Code_14889.java
@@ -0,0 +1,67 @@
+package baekjoon_step.step15_Backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Code_14889 {
+    public static int answer = 19001;   //  양팀간의 케미합 차이
+    public static boolean[] choice;
+    public static int n;
+    public static int[][] chemi;
+    public static int chemiSum;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        choice = new boolean[n];    //  1팀으로 선택된 사람들
+        chemi = new int[n][n];
+        chemiSum = 0;   //  케미의 전체 합
+
+        /*팀원 i와 j의 케미 입력*/
+        for (int i = 0; i < n; ++i) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; ++j) {
+                chemi[i][j] = Integer.parseInt(st.nextToken());
+                chemiSum += chemi[i][j];
+            }
+        }
+        division(0, 0);
+        System.out.print(answer);
+
+        br.close();
+    }
+
+    private static void division(int start, int deep) {
+        /* n/2명의 팀원을 다 골랐다면 각 팀의 전력 비교 */
+        if (deep == n / 2) {
+            int team1 = 0;
+            int team2 = 0;
+            for (int i = 0; i < n; ++i) {
+                for (int j = i + 1; j < n; ++j) {
+                    if (choice[i] && choice[j]) {
+                        team1 += chemi[i][j];
+                        team1 += chemi[j][i];
+                    } else if (!choice[i] && !choice[j]) {
+                        team2 += chemi[i][j];
+                        team2 += chemi[j][i];
+                    }
+                }
+            }
+            /*두 팀의 전력 차이 비교하기*/
+            if (Math.abs(team1 - team2) < answer)
+                answer = Math.abs(team1 - team2);
+            return;
+        }
+
+        /* 팀원을 중복되지 않게 한명씩 고르기 */
+        for (int i = start; i < n; ++i) {
+            choice[i] = true;
+            division(i+1,deep + 1);
+            choice[i] = false;
+        }
+    }
+}
