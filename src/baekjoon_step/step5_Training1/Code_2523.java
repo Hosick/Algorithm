a2e5cd5e477aa584ed3d520da2489bd8f34ce64b
Hosick <ghtjr5000@naver.com>
2020-03-27 21:20:12 +0900

    simple Algorithm

---
+++ src/baekjoon_step/step5_Training1/Code_2523.java
@@ -0,0 +1,25 @@
+package baekjoon_step.step5_Training1;
+
+import java.util.Scanner;
+
+public class Code_2523 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = n-1; i > 0; --i){
+            for(int j = i; j > 0; --j){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
