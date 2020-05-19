9f66ff6703fa3c1c705ff59d4d4eb2cfbeb0f39c
Hosick <ghtjr5000@naver.com>
2020-05-19 17:44:43 +0900

    simple sort

--- src/baekjoon/sort/Code_3047.java
+++ src/baekjoon/sort/Code_3047.java
@@ -4,32 +4,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Code_3047 {
-    static int[][] resultArray = null;
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int arr[] = new int[3];
 
-        arr[0] = input.nextInt();
-        arr[1] = input.nextInt();
-        arr[2] = input.nextInt();
-
-        String str = input.nextLine().trim();
-
-        Arrays.sort(arr);
-
-        for (int i = 0; i < 3; i++) {
-            char c = str.charAt(i);
-
-            if (c == 'A')
-                System.out.print(arr[0] + " ");
-            if (c == 'B')
-                System.out.print(arr[1] + " ");
-            if (c == 'C')
-                System.out.print(arr[2] + " ");
+        int[] number = new int[3];
+        for (int i = 0; i < 3; ++i) {
+            number[i] = input.nextInt();
         }
+
+        Arrays.sort(number);
+
+        String s = input.next();
+
+        for (int i = 0; i < 3; ++i) {
+            if (s.charAt(i) == 'A')
+                System.out.print(number[0]+" ");
+            else if (s.charAt(i) == 'B')
+                System.out.print(number[1]+" ");
+            else
+                System.out.print(number[2]+" ");
+        }
+        System.out.println();
     }
 }
-
-
-출처:https://ktko.tistory.com/entry/백준알고리즘-3047번-ABC [KTKO 개발 블로그와 여행 일기]
