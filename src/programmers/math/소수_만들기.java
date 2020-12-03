c81657dd16a04c1dcbdf42e38776571616bc555a
Hosick <ghtjr5000@naver.com>
2020-12-03 16:36:38 +0900

    feat: math

---
+++ src/programmers/math/소수_만들기.java
@@ -0,0 +1,27 @@
+package programmers.math;
+
+class 소수_만들기 {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length - 2; ++i) {
+            for (int j = i + 1; j < nums.length - 1; ++j) {
+                for (int k = j + 1; k < nums.length; ++k) {
+                    if (isPrime(nums[i] + nums[j] + nums[k]))
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public boolean isPrime(int n) {
+        if (n == 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); ++i) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
